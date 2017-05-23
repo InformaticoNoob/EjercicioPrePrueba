5ab78ab703151225bb4b1cbd30cbdd5895d83cb0
cetecom <cetecom@LC08L5706>
2017-05-23 11:12:22 -0400

    ..

--- Formulario.php
+++ Formulario.php
@@ -1,8 +1,18 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <form method="post" action="modulo/Agregar.php">
+            <div><label>Nombre:</label><input type="text" name="nombre"></div>
+            <div><label>Total Usd:</label><input type="text" name="totalusd"></div>
+            <div><label>Año:</label><input type="text" name="ano"></div>
+            <input type="submit">
+        </form>
+        
+        
+        
+    </body>
+ 
+</html>
--- lib/Producto.php
+++ lib/Producto.php
@@ -2,16 +2,19 @@
 
 class Producto
 {
+    
     var $nombre='';
-    var $precio=0;
-    public $codigo="";
+    var $totalusd=0;
+    var $año='';
+    public $idproducto="";
     
     /*Constructor de la Clase */
-    public function __construct($nombre="",$precio=0,$codigo='') 
+    public function __construct($nombre="",$totalusd=0,$año='',$idproducto='') 
     {
         $this->nombre=$nombre;
-        $this->precio=$precio;
-        $this->codigo=$codigo;
+        $this->totalusd=$totalusd;
+        $this->año=$año;
+        $this->idproducto=$idproducto;
     }
     
     
