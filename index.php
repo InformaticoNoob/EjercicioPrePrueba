0a1331381ba3082984175edd9114318c387637c2
cetecom <cetecom@LC08L5706>
2017-05-23 11:06:28 -0400

    ---

--- index.php
+++ index.php
@@ -1,12 +1,36 @@
+<?php
+session_start();
 
+?>
 <html>
     <head>
         <meta charset="UTF-8">
         <title></title>
     </head>
     <body>
+        <div> 
+            <?php
+        if(!isset($_SESSION['USR'])) 
+        {
+            ?> 
+            <a href="cerrar.php">Cerrar Sesion</a> 
+        </div>
         <?php
+        }
+        ?>
         
+        <a href="sesion.php">Revisi&oacute;n sesi&oacute;n</a>
+        <?php
+            if(!isset($_SESSION['USR']))
+            {
+        ?>
+        <form action="sesion.php" method="post" >
+            <div><label>Usuario:</label><input type="text" name="nombre"></div>
+            <div><label>Clave:</label><input type="text" name="clave"></div>
+            <input type="submit" value="Acceder">
+        </form>
+        <?php
+            }
         ?>
     </body>
 </html>
