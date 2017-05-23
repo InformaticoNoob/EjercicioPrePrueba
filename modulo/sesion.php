c96b51a1f933ba806b66181b943fface10215b1c
cetecom <cetecom@LC08L5706>
2017-05-23 10:54:03 -0400

    ...

---
+++ modulo/sesion.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+
+$PATH='lib/';
+
+include $PATH.'Conexion.php';
+include $PATH.'Usuario.php';
+
+$oUsr=new Usuario();
+
+$oUsr->nombre=$_POST['nombre'];
+$oUsr->clave=$_POST['clave'];
+
+if($oUsr->VerificaUsuarioClave())
+
+    $_SESSION['USR']=$oUsr;   
+
+header('location:http://localhost:8081/Ejercicio1/');
+
+?>
